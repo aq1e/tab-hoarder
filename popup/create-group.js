e4c5b69bbca0affafdc40f4541c8d88d4f84ad24
aq1e <me@ellie.monster>
2023-10-28 22:18:24 -0400

    Use current date & time as default group name

--- popup/create-group.js
+++ popup/create-group.js
@@ -1,3 +1,8 @@
+// use date+time as default group name 
+const date = new Date();
+const dateTimeString = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+document.getElementById("create-group-name").placeholder = dateTimeString;
+
 const tabs = await chrome.tabs.query({ currentWindow: true});
 const template = document.getElementById("tab-li-template");
 const elements = new Set();
@@ -18,7 +23,9 @@ for (const tab of tabs) {
 document.querySelector("#tab-list").append(...elements);
 
 document.querySelector("#submit-button").addEventListener("click", async () => {
-    const groupName = document.querySelector("#create-group-name").value;
+    let groupName = document.querySelector("#create-group-name").value;
+    if (!groupName) { groupName = dateTimeString; }
+    console.log(groupName);
     const groupFolder = await chrome.bookmarks.create({'title': groupName });
     document.querySelectorAll('.tab').forEach( tab => {
         const title = tab.querySelector('.title-input').value;
